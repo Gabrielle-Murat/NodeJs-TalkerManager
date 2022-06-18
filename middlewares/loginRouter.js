a9de849759bd3b71b427b91e28d8276d26701ac1
Gabrielle-Murat <belle.murat@gmail.com>
2022-06-18 20:04:35 -0300

    Adiciona validacoes de login ao router

--- middlewares/loginRouter.js
+++ middlewares/loginRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
 const tokenGenerator = require('../utils/token-generation-utils');
+const validation = require('./validations');
 
 const router = express.Router();
 
-router.post('/', (_req, res) => {
+router.post('/', validation, (_req, res) => {
   const token = tokenGenerator();
   res.status(200).json({ token: `${token}` });
 });
