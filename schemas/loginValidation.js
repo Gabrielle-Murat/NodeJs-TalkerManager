98a8fcb1d3dc663b6dde8c6ae042032aaeeb3b34
Gabrielle-Murat <belle.murat@gmail.com>
2022-06-18 18:54:03 -0300

    Cria esquemas de validacao para login

---
+++ schemas/loginValidation.js
@@ -0,0 +1,23 @@
+import Joi from 'joi';
+
+const loginValidation = Joi.object({
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ['com'] } })
+    .required()
+    .messages({
+      'any.required': '400|O campo "email" é obrigatório',
+      'string.base': '400|O "email" deve ser uma string',
+      'string.email': '400|O "email" deve ter o formato "email@email.com"',
+    }),
+  
+  password: Joi.string()
+    .min(6)
+    .required()
+    .messages({
+      'any.required': '400|O campo "password" é obrigatório',
+      'string.base': '400|O "password" deve ser uma string',
+      'string.min': '400|O "password" deve ter pelo menos 6 caracteres',
+    }),
+});
+
+export default loginValidation;
