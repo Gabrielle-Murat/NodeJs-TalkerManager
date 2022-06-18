479889cc3951e48c2e4b2db00dd4486500a93128
Gabrielle-Murat <belle.murat@gmail.com>
2022-06-18 17:12:13 -0300

    Cria um endpoint get para buscar todos os talkers

--- talkersRouter.js
+++ talkersRouter.js
@@ -1,5 +1,13 @@
 const express = require('express');
 
+const talkerUtils = require('./fs-utils');
+
 const router = express.Router();
+
+router.get('/', async (_req, res) => {
+  const talkers = await talkerUtils.getTalker();
+
+  return res.status(200).json(talkers);
+});
 
 module.exports = router;
