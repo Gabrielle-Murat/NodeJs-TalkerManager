80d534878f7181e3fddd674ec08640516651b8c5
Gabrielle-Murat <belle.murat@gmail.com>
2022-06-18 17:34:35 -0300

    Cria um endpoint get para buscar um talker pelo id

--- talkersRouter.js
+++ talkersRouter.js
@@ -10,4 +10,14 @@ router.get('/', async (_req, res) => {
   return res.status(200).json(talkers);
 });
 
+router.get('/:id', async (req, res) => {
+  const talkers = await talkerUtils.getTalker();
+
+  const talker = talkers.find(({ id }) => id === parseInt(req.params.id, 10));
+
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  return res.status(200).json(talker);
+});
+
 module.exports = router;
