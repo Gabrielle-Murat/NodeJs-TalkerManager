758facfea8a585cec532daccd4154ff2fa28451b
Gabrielle-Murat <belle.murat@gmail.com>
2022-06-20 11:01:45 -0300

    Remove comentarios

--- middlewares/loginValidation.js
+++ middlewares/loginValidation.js
@@ -2,12 +2,9 @@ const loginData = require('../schemas/loginValidations');
 
 function loginValidation(req, res, next) {
   const { error } = loginData.validate(req.body);
-  // console.log(error);
-  // console.log(req.body);
 
   if (error) {
     const [code, message] = error.message.split('|');
-    // console.log(code);
 
     return res.status(parseInt(code, 10)).json({ message });
   }
